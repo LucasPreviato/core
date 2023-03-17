27d2d04655fa43f535cfb15e1386cfa66a4c25b8
Lucas Previato <lucaspreviato@Live.com>
2023-03-17 01:59:28 +0000

    feat(department):implements dtos

--- src/departments/dto/create-department.dto.ts
+++ src/departments/dto/create-department.dto.ts
@@ -1,1 +1,20 @@
-export class CreateDepartmentDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsOptional,
+  MinLength,
+} from 'class-validator';
+
+export class CreateDepartmentDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  abbreviation?: string;
+  @IsNumber()
+  @IsNotEmpty()
+  unitId: number;
+}
