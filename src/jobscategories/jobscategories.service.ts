23ec006e3adc8f0a2921a8770407f9352a07cda1
Lucas Previato <lucaspreviato@Live.com>
2023-03-24 02:35:09 +0000

    feat(jobscategories):implements services

--- src/jobscategories/jobscategories.service.ts
+++ src/jobscategories/jobscategories.service.ts
@@ -1,26 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { CreateJobscategoryDto } from './dto/create-jobscategory.dto';
 import { UpdateJobscategoryDto } from './dto/update-jobscategory.dto';
+import { PrismaJobsCategoriesRepository } from './repositories/prisma/prisma.jobscategories.repository';
 
 @Injectable()
 export class JobscategoriesService {
-  create(createJobscategoryDto: CreateJobscategoryDto) {
-    return 'This action adds a new jobscategory';
+  constructor(
+    @InjectPinoLogger(JobscategoriesService.name)
+    private readonly logger: PinoLogger,
+    private readonly prisma: PrismaJobsCategoriesRepository,
+  ) {}
+
+  async create({ name }: CreateJobscategoryDto): Promise<void> {
+    this.logger.info(`Creating jobscategory with name: ${name}`);
+    const jobscategory = await this.prisma.create({ name });
+    this.logger.info(`Created jobscategory in database with name: ${name}`);
   }
 
-  findAll() {
-    return `This action returns all jobscategories`;
+  async findAll(): Promise<void> {
+    const jobscategories = await this.prisma.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} jobscategory`;
+  async findOne(id: number): Promise<void> {
+    this.logger.info(`Finding jobscategory with id: ${id}`);
+    const jobscategory = await this.prisma.findOne(id);
+    if (!jobscategory) {
+      this.logger.error(`Jobscategory with id: ${id} not found`);
+      throw new Error(`Jobscategory with id: ${id} not found`);
+    }
+    this.logger.info(`Found jobscategory with id: ${id}`);
   }
 
-  update(id: number, updateJobscategoryDto: UpdateJobscategoryDto) {
-    return `This action updates a #${id} jobscategory`;
+  async update(
+    id: number,
+    updateJobscategoryDto: UpdateJobscategoryDto,
+  ): Promise<void> {
+    this.logger.info(`Updating jobscategory with id: ${id}`);
+    const jobscategory = await this.prisma.update(id, updateJobscategoryDto);
+    if (!jobscategory) {
+      this.logger.error(`Jobscategory with id: ${id} not found`);
+      throw new Error(`Jobscategory with id: ${id} not found`);
+    }
+    this.logger.info(`Updated jobscategory with id: ${id}`);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} jobscategory`;
+  async remove(id: number): Promise<void> {
+    this.logger.info(`Removing jobscategory with id: ${id}`);
+    const jobscategory = await this.prisma.remove(id);
+    if (!jobscategory) {
+      this.logger.error(`Jobscategory with id: ${id} not found`);
+      throw new Error(`Jobscategory with id: ${id} not found`);
+    }
+    this.logger.info(`Removed jobscategory with id: ${id}`);
   }
 }
