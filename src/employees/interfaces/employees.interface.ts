b24ff99f6ee2094dcda005117e5ca175a936e8c5
Lucas Previato <lucaspreviato@Live.com>
2023-03-25 00:44:35 +0000

    feat(Employess):implements interface

--- src/employees/interfaces/employees.interface.ts
+++ src/employees/interfaces/employees.interface.ts
@@ -1,6 +1,23 @@
+import { IDepartment } from 'src/departments/interfaces/departments.interface';
+import { IJobTitles } from 'src/jobstitles/interfaces/jobstitles.interface';
+import { IUnit } from 'src/units/interfaces/unit.interface';
 
+export enum EmployeeStatus {
+  ACTIVE = 'active',
+  EXPERIENCE = 'experience',
+  INACTIVE = 'inactive',
+}
 
 export interface IEmployee {
-    id: number;
-    
+  id: number;
+  name: string;
+  email: string;
+  phone?: string;
+  status: EmployeeStatus;
+  hireDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  unit: IUnit;
+  department: IDepartment;
+  jobTitle: IJobTitles;
 }
