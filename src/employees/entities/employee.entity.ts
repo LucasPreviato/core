305d5376380e789f454b63bdec1935b19adc8168
Lucas Previato <lucaspreviato@Live.com>
2023-03-25 00:45:00 +0000

    feat(Employess):implements entity

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,1 +1,18 @@
-export class Employee {}
+import { IDepartment } from 'src/departments/interfaces/departments.interface';
+import { IJobTitles } from 'src/jobstitles/interfaces/jobstitles.interface';
+import { IUnit } from 'src/units/interfaces/unit.interface';
+import { EmployeeStatus, IEmployee } from '../interfaces/employees.interface';
+
+export class Employee implements IEmployee {
+  id: number;
+  name: string;
+  email: string;
+  phone?: string;
+  status: EmployeeStatus;
+  hireDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  unit: IUnit;
+  department: IDepartment;
+  jobTitle: IJobTitles;
+}
